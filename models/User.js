7a8b38ed9be94007e1b3baf0609146dd07291c79
meir <lightir21@gmail.com>
2022-12-13 00:42:59 +0200

    bug

---
+++ models/User.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide name"],
+    maxlength: 20,
+    minlength: 3,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "please provide valid email",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "please provide password"],
+    minlength: 6,
+    select: false,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "lastName",
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "my city",
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
+
+export default mongoose.model("User", UserSchema);
